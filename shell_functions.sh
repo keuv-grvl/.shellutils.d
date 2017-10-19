de186e6fd64fd87096f1fd16a07c3de04e46c94d
Kévin Gravouil <k.gravouil@gmail.com>
2017-10-19 16:00:40 +0200

    now compliant with ShellCheck

--- shell_functions.sh
+++ shell_functions.sh
@@ -3,12 +3,12 @@
 
 # make and go to dir
 function mkcd {
-  mkdir -p $1 && cd $1
+  mkdir -p "$1" && cd "$1"
 }
 
 # make dir into RAM (avoiding conflict with other users)
 function cdmem {
-  mkdir -p /dev/shm/$USER && cd /dev/shm/$USER
+  mkdir -p "/dev/shm/$USER" && cd "/dev/shm/$USER"
 }
 
 # print status code of the last command
@@ -18,59 +18,63 @@ function ret {
 
 # load = (loadavg / nproc * 100)
 function load {
-  echo $(awk '{print $1}' /proc/loadavg )" "$(nproc) | awk '{ printf "%.2f%% (%.2f/%d)\n", $1/$2*100, $1, $2 }'
+  echo "$(awk '{print $1}' /proc/loadavg )" "$(nproc)"  \
+  | awk '{ printf "%.2f%% (%.2f/%d)\n", "$1"/$2*100, "$1", $2 }'
 }
 
 # memory usage (as in gnome-system-monitor, cached memory is not considered)
 function memu {
-  free | awk 'NR==2{print}' | sed -r 's/\s{2,}/\t/g' | awk -F "\t" '{print $0; printf "%.2f%% (%.2f/%.2fGB)\n", ($3 / $2), $3/1024/1024, $2/1024/1024}'
+  free  \
+  | awk 'NR==2{print}'  \
+  | sed -r 's/\s{2,}/\t/g'  \
+  | awk -F "\t" '{print $0; printf "%.2f%% (%.2f/%.2fGB)\n", ($3 / $2), $3/1024/1024, $2/1024/1024}'
 }
 
 # disk usage (of $HOME by default)
 function disku {
   DIR=$HOME
-  [ ! -z $1 ] && DIR=$1
-  df -P $DIR | awk 'NR==2{printf("%.2f%% (%.2f/%.2fGB)\n", $3/$2*100, $3/1024/1024 , ($2)/1024/1024)}'
+  [ ! -z "$1" ] && DIR="$1"
+  df "$DIR"  \
+  | awk 'NR==2{printf("%.2f%% (%.2f/%.2fGB)\n", $3/$2*100, $3/1024/1024 , ($2)/1024/1024)}'
 }
 
 # download using curl
 function dl {
-  curl -OL $@
+  curl -OL "$@"
 }
 
 # max number of "free" CPU = max(1, (number of processor - current load))
 function maxproc {
   nproc=$(grep -c "processor" /proc/cpuinfo)
-  load=$(cat /proc/loadavg | awk '{printf "%.0f\n", ($1 + 0.5)}')
-  maxproc=$(( $nproc - $load ))
-  [ $maxproc -lt 1 ] && echo 1 || echo $(($maxproc))
+  load=$(awk '{printf "%.0f\n", ($1 + 0.5)}' /proc/loadavg)
+  maxproc=$(( nproc - load ))
+  [ $maxproc -lt 1 ] && echo 1 || echo $maxproc
 }
 
 # compute and store MD5hash of a list of file
 function hashall {
-  echo $@ \
-  | tr ' ' '\n' \
-  | grep -vE ".md5$" \
-  | xargs -I F -P$(maxproc) \
+  echo "$@"  \
+  | tr ' ' '\n'  \
+  | grep -vE ".md5$"  \
+  | xargs -I F -P"$(maxproc)"  \
     bash -c "echo '- F' ; md5sum F > F.md5"
 }
 
 # check MD5sum from .md5 file (usage: checkall *.md5)
 function checkall {
-  echo $@ \
-  | tr ' ' '\n' \
-  | xargs -I F -P$(maxproc) \
+  echo "$@"  \
+  | tr ' ' '\n'  \
+  | xargs -I F -P"$(maxproc)"  \
     bash -c "md5sum -c F"
 }
 
 # list hostnames in you ~/.ssh/config and ping them
 # - Some servers has been configured to not respond to ping
 function pingssh {
-  cat $HOME/.ssh/config \
-  | grep -i "hostname" \
-  | awk '{print $NF}' \
-  | xargs -I IP -n1 -P30 \
-    bash -c "ping -c1 IP 2>&1 >/dev/null && echo -e 'IP\treachable' || echo -e 'IP\tnot reachable'"
+  grep -i "hostname" "$HOME/.ssh/config"  \
+  | awk '{print $NF}'  \
+  | xargs -I IP -n1 -P30  \
+    bash -c "ping -c1 IP 2>&1 >/dev/null && echo -e '\e[32m⚫\e[0m IP' || echo -e '\e[31m❌\e[0m IP'"
 }
 
 # extract any archive
@@ -98,7 +102,7 @@ function extract {
 
 # convert a .md file to PDF using pandoc
 function md2pdf {
-  fullfile=$1
+  fullfile="$1"
   filename=$(basename "$fullfile")
   OUT="${filename%.*}"
   pandoc -s -V geometry:margin=1in -V documentclass:article -V fontsize=12pt "$fullfile" -o "$OUT.pdf"
@@ -108,23 +112,25 @@ function md2pdf {
 function gpuload {
   which nvidia-smi >/dev/null 2>&1  \
   && ( nvidia-smi --query-gpu=utilization.gpu,utilization.memory --format=csv,noheader,nounits  \
-       | awk -F", " '{print "GPU load: "$1"%\nGPU mem. usage: "$2"%"}' )  \
-  || echo "No gpu found"
+       | awk -F", " '{print "GPU load: ""$1""%\nGPU mem. usage: "$2"%"}' )  \
+  || echo "No GPU found"
 }
 
-
 # deeply inspired from R package beepR
-# usage: beep list ; beep coin ; beep treasure
+# usage: beep list ; beep coin ; sleep 4 && beep treasure
 function beep {
   PLAYER="paplay"
-  [ $(uname) = "Darwin" ] && PLAYER="afplay"
+  [ "$(uname)" = "Darwin" ] && PLAYER="afplay"
   SOUNDS_DIR="$HOME/.shellutils.d/sounds"
   DEFAULT_SOUND="ping.wav"
 
-  [ "$1" = "list" ] && echo $(ls -1 $SOUNDS_DIR | sed 's/\.wav//') \
-  || (
-      [ -f "$SOUNDS_DIR/$1.wav" ] \
-        && $PLAYER "$SOUNDS_DIR/$1.wav" \
-        || $PLAYER "$SOUNDS_DIR/$DEFAULT_SOUND"
-      )
+  if [ "$1" = "list" ] ; then
+    find "$SOUNDS_DIR" -maxdepth 1 -mindepth 1 -type f -exec sh -c 'echo $(basename {} .wav)' \;
+  else
+    if [ -f "$SOUNDS_DIR/$1.wav" ] ; then
+      $PLAYER "$SOUNDS_DIR/$1.wav"
+    else
+      $PLAYER "$SOUNDS_DIR/$DEFAULT_SOUND"
+    fi
+  fi
 }
