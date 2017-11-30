33dc7302dc9f4b196911c6172589f2b23a5fd750
Kévin Gravouil <k.gravouil@gmail.com>
2017-11-30 11:26:40 +0100

    fix load

--- shell_functions.sh
+++ shell_functions.sh
@@ -18,8 +18,8 @@ function ret {
 
 # load = (loadavg / nproc * 100)
 function load {
-  echo "$(awk '{print $1}' /proc/loadavg )" "$(nproc)"  \
-  | awk '{ printf "%.2f%% (%.2f/%d)\n", "$1"/$2*100, "$1", $2 }'
+   echo "$(awk '{print $1}' /proc/loadavg )" "$(nproc)"  \
+  | awk '{ printf("%.2f%% (%.2f/%i)", $1/$2*100, $1, $2) }'
 }
 
 # memory usage (as in gnome-system-monitor, cached memory is not considered)
